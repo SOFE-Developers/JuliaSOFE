d8ceb682bace73a7a4aa9a275c771641b5bf4afd
Andreas Kunze <andreas.kunze@mailbox.tu-dresden.de>
2016-10-18 13:30:38 +0200

    added Lagrange Q1 element type

--- src/elements/Elements.jl
+++ src/elements/Elements.jl
@@ -96,6 +96,94 @@ type LagrangeQ1 <: AbstractElement
     LagrangeQ1(dim::Integer) = new(Element(dim, collect(1:dim+1), collect(1:dim+1), 1))
 end
 
+# Associated Methods
+# -------------------
+function evalD0Basis{T<:Float}(el::LagrangeQ1, points::AbstractArray{T,2})
+    nP, nD = size(points)
+    size(el.super, nD)
+    B = zeros(nB, nP, 1)
+    if nD == 1
+        B[1,:] = 1-points[:,1];
+        B[2,:] = points[:,1];
+    elseif nD == 2
+        B[1,:] = (1-points[:,1]).*(1-points[:,2]);
+        B[2,:] = points[:,1].*(1-points[:,2]);
+        B[3,:] = (1-points[:,1]).*points[:,2];
+        B[4,:] = points[:,1].*points[:,2];
+    elseif nD == 3
+        B[1,:] = (1-points[:,1]).*(1-points[:,2]).*(1-points[:,3]);
+        B[2,:] = points[:,1].*(1-points[:,2]).*(1-points[:,3]);
+        B[3,:] = (1-points[:,1]).*points[:,2].*(1-points[:,3]);
+        B[4,:] = points[:,1].*points[:,2].*(1-points[:,3]);
+        B[5,:] = (1-points[:,1]).*(1-points[:,2]).*points[:,3];
+        B[6,:] = points[:,1].*(1-points[:,2]).*points[:,3];
+        B[7,:] = (1-points[:,1]).*points[:,2].*points[:,3];
+        B[8,:] = points[:,1].*points[:,2].*points[:,3];
+    else
+        error("Not supported size of points")
+    end
+    return B
+end
+
+function evalD1Basis{T<:Float}(el::LagrangeQ1, points::AbstractArray{T,2})
+    (nP, nD) = size(points)
+    B = zeros(el.super.nB[nD], nP, 1, nD)
+    if nD == 1
+        B[1,:,:,1] = -1;
+        B[2,:,:,1] = 1;
+    elseif nD == 2
+        B[1,:,:,1] = -(1-points[:,2]);
+        B[1,:,:,2] = -(1-points[:,1]);
+        B[2,:,:,1] = 1-points[:,2];
+        B[2,:,:,2] = -points[:,1];
+        B[3,:,:,1] = -points[:,2];
+        B[3,:,:,2] = 1-points[:,1];
+        B[4,:,:,1] = points[:,2];
+        B[4,:,:,2] = points[:,1];
+    elseif nD == 3
+        B[1,:,:,1] = -(1-points[:,2]).*(1-points[:,3]);
+        B[1,:,:,2] = -(1-points[:,1]).*(1-points[:,3]);
+        B[1,:,:,3] = -(1-points[:,1]).*(1-points[:,2]);
+
+        B[2,:,:,1] = (1-points[:,2]).*(1-points[:,3]);
+        B[2,:,:,2] = -points[:,1].*(1-points[:,3]);
+        B[2,:,:,3] = -points[:,1].*(1-points[:,2]);
+
+        B[3,:,:,1] = -points[:,2].*(1-points[:,3]);
+        B[3,:,:,2] = (1-points[:,1]).*(1-points[:,3]);
+        B[3,:,:,3] = -(1-points[:,1]).*points[:,2];
+
+        B[4,:,:,1] = points[:,2].*(1-points[:,3]);
+        B[4,:,:,2] = points[:,1].*(1-points[:,3]);
+        B[4,:,:,3] = -points[:,1].*points[:,2];
+
+        B[5,:,:,1] = -(1-points[:,2]).*points[:,3];
+        B[5,:,:,2] = -(1-points[:,1]).*points[:,3];
+        B[5,:,:,3] = (1-points[:,1]).*(1-points[:,2]);
+        
+        B[6,:,:,1] = (1-points[:,2]).*points[:,3];
+        B[6,:,:,2] = -points[:,1].*points[:,3];
+        B[6,:,:,3] = points[:,1].*(1-points[:,2]);
+        
+        B[7,:,:,1] = -points[:,2].*points[:,3];
+        B[7,:,:,2] = (1-points[:,1]).*points[:,3];
+        B[7,:,:,3] = (1-points[:,1]).*points[:,2];
+        
+        B[8,:,:,1] = points[:,2].*points[:,3];
+        B[8,:,:,2] = points[:,1].*points[:,3];
+        B[8,:,:,3] = points[:,1].*points[:,2];
+    else
+        error("Not supported size of points")
+    end
+    return B
+end
+
+function evalD2Basis{T<:Float}(el::LagrangeQ1, points::AbstractArray{T,2})
+  (nP, nD) = size(points)
+  B = zeros(el.super.nB[nD], nP, 1, nD, nD)
+  return B
+end
+
 
 end # of module Elements
 
