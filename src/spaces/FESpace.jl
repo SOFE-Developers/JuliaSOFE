0c53bbea7499be0bc21fb0d5d7d49dfe1e9e46af
Andreas Kunze <andreas.kunze@mailbox.tu-dresden.de>
2016-10-20 10:14:44 +0200

    implemented generic dof mapping using element doftuples

--- src/spaces/FESpace.jl
+++ src/spaces/FESpace.jl
@@ -25,22 +25,26 @@ function getDOFMap(fes::FESpace, d::Integer)
     dofTuple = fes.element.dofTUple
     dofPerDim = fes.element.dofPerDim
     nEntities = [getNumber(fes.mesh.topology, dd) for dd = 0:d]
-    dofsNeeded = [nEntities[dd+1] * dofTuple[dd+1]
-                  for dd = 0:d]
+    dofsNeeded = [nEntities[dd+1] * dofTuple[dd+1] for dd = 0:d]
     ndofs = [0, cumsum(dofsNeeded)...]
     dofs = [reshape(ndofs[i]+1:ndofs[i+1], dofTuple[i], nEntities[i]) for i = 1:d+1]
 
-    dofMap = [zeros(Int, dofPerDim[i], nEntities[d]) for i = 1:d+1]
+    dofMap = [zeros(Int, dofPerDim[i], nEntities[d+1]) for i = 1:d+1]
     # first iterate over subdims
     for dd = 0:d-1
         for (i, d_dd_i) in enumerate(getConnectivity(fes.mesh.topology, d, dd))
+            println(d_dd_i)
             for (j, d_dd_ij) in enumerate(d_dd_i)
-                dofMap[dd][:,i] = ...
+                r = (j-1)*dofTuple[dd+1]+1 : j*dofTuple[dd+1]
+                dofMap[dd+1][r,i] = dofs[dd+1][:,j]
             end
         end
     end
 
-    
+    # set dofs of query dim
+    dofMap[d+1] = dofs[d+1]
+
+    return vcat(dofMap...)
 end
 
 end # of module Spaces
