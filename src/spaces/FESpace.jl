f6956d59ba9f6f57caec05da8310e4fa5510e139
Andreas Kunze <andreas.kunze@mailbox.tu-dresden.de>
2016-11-16 15:00:54 +0100

    added some mixed fe space functionality

--- src/spaces/FESpace.jl
+++ src/spaces/FESpace.jl
@@ -66,7 +66,7 @@ domain!(fes::FESpace, f::Function) = setfield!(fes, :domain, f)
   the constrained domain of the finite element space.
 """
 shift(fes::FESpace) = getfield(fes, :shift)
-shift!(fes:FESpace, f::Function) = setfield!(fes, :shift, f)
+shift!(fes::FESpace, f::Function) = setfield!(fes, :shift, f)
 
 """
 
@@ -145,7 +145,6 @@ end
   finite element space `fes`.
 """
 function nDoF(fes::FESpace)
-    #return maxabs(getDOFMap(fes, fes.mesh.dimension))
     return maxabs(dofMap(fes, d=dimension(mesh(fes))))
 end
 
@@ -207,5 +206,33 @@ function interpolate(m::Mesh, el::Element, f::Function)
     return f(n)
 end
 interpolate(fes::FESpace, f::Function) = interpolate(fes.mesh, fes.element, f)
+
+#-------------------#
+# Type MixedFESpace #
+#-------------------#
+type MixedFESpace <: AbstractFESpace
+    subspaces :: Array{FESpace, 1}
+end
+MixedFESpace(spaces::FESpace...) = MixedFESpace([space for space in spaces])
+
+"""
+
+    subspaces(mfes::MixedFESpace)
+
+  Return the each subspace of the mixed finite element space.
+"""
+subspaces(mfes::MixedFESpace) = getfield(mfes, :subspaces)
+subspace(mfes::MixedFESpace, i::Integer) = subspaces(mfes)[i]
+
+fixedDoF(mfes::MixedFESpace) = mapreduce(fixedDoF, vcat, subspaces(mfes))
+freeDoF(mfes::MixedFESpace) = mapreduce(freeDoF, vcat, subspaces(mfes))
+nDoF(mfes::MixedFESpace) = mapreduce(nDoF, +, subspaces(mfes))
+
+fixedDoF(mfes::MixedFESpace, i::Integer) = fixedDoF(subspace(mfes, i))
+freeDoF(mfes::MixedFESpace, i::Integer) = freeDoF(freeDoF(subspace(mfes, i)))
+nDoF(mfes::MixedFESpace, i::Integer) = nDoF(subspace(mfes, i))
+
+Base.getindex(mfes::MixedFESpace, i::Integer) = subspace(mfes, i)
+Base.length(mfes::MixedFESpace) = length(subspaces(mfes))
 
 end # of module Spaces
