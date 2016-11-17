cba4d2906eda5164abab100b166be3747ffdba4c
Andreas Kunze <andreas.kunze@mailbox.tu-dresden.de>
2016-11-17 12:01:56 +0100

    implemented evaluation of constants on meshes

--- src/meshes/Meshes.jl
+++ src/meshes/Meshes.jl
@@ -94,6 +94,28 @@ Topology.boundary(m::Mesh, f::Function) = boundary(topology(m), f)
 # Reference Maps
 include("refmaps.jl")
 
+typealias Constant{T<:Real} Union{T, AbstractVector{T}, AbstractMatrix{T}}
+
+function evaluate{T<:Constant,S<:Float}(c::T, points::AbstractArray{S,2}, m::Mesh)
+    P = evalReferenceMaps(m, points)
+    nE, nP, nW = size(P)
+
+    if issubtype(T, Real)
+        C = convert(promote_type(eltype(T),S), c)
+    elseif issubtype(T, AbstractVector)
+        C = convert(Vector{promote_type(eltype(T),S)}, c)
+    elseif issubtype(T, AbstractMatrix)
+        C = convert(Matrix{promote_type(eltype(T),S)}, c)
+    end
+    
+    R = zeros(S, nE, nP, size(C)...)
+    for ip = 1:nP
+        for ie = 1:nE
+            R[ie,ip,:] = C
+        end
+    end
+    return R
+end
 
 function evalFunction{T<:Float}(m::Mesh, f::Function, points::AbstractArray{T,2})
     P = evalReferenceMap(m, points) # nExnPxnW
