7356a7ea23675830c98c274490e5e676aed94cf1
Andreas Kunze <andreas.kunze@mailbox.tu-dresden.de>
2016-11-18 09:26:57 +0100

    sync

--- src/postprocessing/Visualization.jl
+++ src/postprocessing/Visualization.jl
@@ -4,6 +4,9 @@ using PyCall
 
 using ..Elements
 using ..Meshes
+using ..Spaces
+
+export show
 
 const pyvisofe = PyNULL()
 
@@ -17,7 +20,27 @@ visgrid{T<:PElement}(el::Element{T}, resolution::Integer) =
 visgrid{T<:QElement}(el::Element{T}, resolution::Integer) =
     UnitOrthotope(Elements.dimension(el), resolution)
 
-function showMesh(m::Mesh)
+function visgrid(m::Mesh, resolution::Integer)
+    refmesh = visgrid(m.element, resolution)
+    refnodes = nodes(refmesh)
+    refcells = entities(refmesh, dimension(refmesh))
+    nE_ref, nV = size(refcells)
+    nN_ref, nW = size(refnodes)
+
+    visnodes = evalReferenceMaps(m, refnodes, 0)
+    nE_vis, nN_ref, nW = size(visnodes)
+
+    viscells = broadcast(+, reshape(refcells, (nE_ref,1,nV)),
+                         reshape(nN_ref * collect(0:nE_vis-1), (1,nE_vis,1)))
+
+    visnodes = permutedims(visnodes, [2,1,3])
+    visnodes = reshape(visnodes, (nE_vis*nN_ref, nW))
+    viscells = reshape(viscells, (nE_ref*nE_vis,nV))
+
+    return Mesh(visnodes, viscells)
+end
+
+function show(m::Mesh)
     D = Meshes.dimension(m)
     
     if D == 1
@@ -41,12 +64,12 @@ function showMesh(m::Mesh)
     end
 end
 
-function showElement(el::Element, i::Integer; resolution::Integer=100,
-                     size::Integer=2, alpha::AbstractFloat=1.)
+function show(el::Element, i::Integer; resolution::Integer=100,
+              size::Integer=2, alpha::AbstractFloat=1.)
     D = Elements.dimension(el)
-    m = visgrid(el, resolution)
-    n = nodes(m)
-    basis = evalBasis(el, n)
+    vismesh = visgrid(el, resolution)
+    n = nodes(vismesh)
+    basis = evalBasis(el, n, 0)
 
     if D == 1
         x = n[:,1]; y = basis[i,:]
@@ -64,5 +87,11 @@ function showElement(el::Element, i::Integer; resolution::Integer=100,
                              size=size, alpha=alpha)
     end
 end
+
+function show{T<:AbstractFloat}(fes::FESpace, uh::AbstractVector{T};
+                                resolution::Integer=3)
+    vismesh_loc = visgrid(element(fes), resolution)
     
+end
+
 end # of module Visualization
