e9e58eabafca949e3f388dca084344048ca96017
Andreas Kunze <andreas.kunze@mailbox.tu-dresden.de>
2016-12-02 17:59:44 +0100

    started newton cotes quadrule

---
+++ src/quadrature/newtoncotes.jl
@@ -0,0 +1,11 @@
+using Jacobi
+
+#-------------------------#
+# Newton Cotes Quadrature #
+#-------------------------#
+type NewtonCotes <: AbstractQuadRule
+end
+
+function NewtonCotesQuadOrth(order::Integer, dim::Integer)
+    
+end
